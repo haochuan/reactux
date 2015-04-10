235c6f6b85c6e7bad8d3c4b30a38c5b714341d5e
Haochuan Liu <haochuan.liu@gmail.com>
2015-04-09 20:44:36 -0700

    update paths

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,8 @@ var streamify = require('gulp-streamify');
 
 var path = {
   HTML: 'src/index.html',
-  MINIFIED_OUT: 'build.min.js',
-  OUT: 'build.js',
+  MINIFIED_OUT: 'app.min.js',
+  OUT: 'app.js',
   DEST: 'dist',
   DEST_BUILD: 'dist/build',
   DEST_SRC: 'dist/src',
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/dist'));
 
 var server = app.listen(3000, function () {
 
