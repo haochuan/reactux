a406dfea049782e243b61d9820ea70b1d1fb51dd
Haochuan Liu <haochuan.liu@gmail.com>
2015-04-09 20:02:35 -0700

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/app'));
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Server listening at http://%s:%s', host, port);
+
+});
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
