b4d2dcd79b2bbcc20b8ec028849c64ae78d7f553
Haochuan Liu <haochuan.liu@gmail.com>
2015-06-16 14:09:56 -0700

    updated

---
+++ src/js/actions/AppActions.js
@@ -0,0 +1,1 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
---
+++ src/js/stores/AppStore.js
@@ -0,0 +1,3 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
