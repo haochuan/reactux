0e5afb41f522da445d788718818669c0f54e0cfa
Haochuan Liu <haochuan.liu@gmail.com>
2015-12-06 12:08:28 -0800

    add more configurations on server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var fs = require('fs');
 var app = express();
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var port = process.env.PORT || 3000;
 
 var router = express.Router({
     caseSensitive: app.get('case sensitive routing'),
@@ -11,11 +13,36 @@ var router = express.Router({
 // Parse application/json
 app.use(bodyParser.json());
 
+/*===========================================
+=            Baic Authentication            =
+===========================================*/
+
+// app.use(require('node-basicauth')({'haochuan': 'password'}));
+
+/*=====  End of Baic Authentication  ======*/
+
+/*====================================
+=            Basic Logger            =
+====================================*/
+
+// app.use(morgan('tiny'));
+
+/*=====  End of Basic Logger  ======*/
+
+/*===========================
+=            COR            =
+===========================*/
+
+app.use(require('cors')());
+
+/*=====  End of COR  ======*/
+
+
 app.use(router);
 
 app.use(express.static('dist', __dirname + '/dist'));
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
 
     var host = server.address().address;
     var port = server.address().port;
@@ -29,7 +56,3 @@ router.get('/', function (req, res) {
   res.sendfile('dist/index.html');
 });
 
-router.get('/data', function(req, res) {
-    var data = fs.readFileSync('data/data.json');
-    res.json(JSON.parse(data));
-});
