0d323f082656fdefdcf723630d3c60134fe061ef
Haochuan Liu <haochuan.liu@gmail.com>
2015-12-17 15:04:17 -0800

    cli paramsters

--- bin/reactGenerator.js
+++ bin/reactGenerator.js
@@ -1,4 +1,17 @@
 #! /usr/bin/env node
-var reactGenerator = require('../lib/generateStructure');
 
-reactGenerator("test");
+var program = require('commander'),
+    gs = require('../lib/generateStructure');
+
+program
+  .version(require('../package.json').version)
+  .usage('[options] [project name]')
+  .parse(process.argv);
+
+var pname = program.args[0]
+
+if (!pname) {
+    program.help();
+} else {
+    gs(pname);
+}
