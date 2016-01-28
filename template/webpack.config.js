de8320ebe645798c2319ad60e4e88c7e84d9fed0
Haochuan Liu <haochuan.liu@gmail.com>
2016-01-27 21:45:09 -0800

    first webpack version

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: [
         'webpack-hot-middleware/client',
-        './index'
+        './src'
     ],
     output: {
         path: path.join(__dirname, 'dist'),
@@ -19,16 +19,34 @@ module.exports = {
     ],
     module: {
         loaders: [
+            // js
             {
                 test: /\.js$/,
                 loaders: [ 'babel' ],
                 exclude: /node_modules/,
                 include: __dirname
             },
-            {
-                test: /\.sass$/,
-                loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
-            }
+            // json
+            { 
+                test: /\.json$/, 
+                loader: 'json-loader' 
+            },
+            // less
+            { 
+                test: /\.less$/, 
+                loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!less?outputStyle=expanded&sourceMap' 
+            },
+            // sass
+            { 
+                test: /\.scss$/, 
+                loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap' 
+            },
+            // font and svg
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
         ] 
     }
 }
