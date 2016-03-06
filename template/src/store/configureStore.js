01778345ff579a7f9743820d39814b1da83c330f
Haochuan Liu <haochuan.liu@gmail.com>
2016-03-05 20:02:59 -0800

    add thunk and logger middleware

--- template/src/store/configureStore.js
+++ template/src/store/configureStore.js
@@ -1,9 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/reducer';
 import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+const logger = createLogger();
 
 export function configureStore(initialState) {
-    const store = (window.devToolsExtension ? window.devToolsExtension()(createStore) : createStore)(rootReducer, initialState);
+    let middlewares = [
+        applyMiddleware(thunk, logger)
+      ];
+    // const store = (window.devToolsExtension ? window.devToolsExtension()(createStore(rootReducer, initialState, applyMiddleware(thunk, logger))) : createStore)(rootReducer, initialState, applyMiddleware(thunk, logger));
+    const store = compose(
+        applyMiddleware(thunk, logger),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )(createStore)(rootReducer, initialState);
+
     if (module.hot) {
         // Enable Webpack hot module replacement for reducers
         module.hot.accept('../reducers/reducer', () => {
