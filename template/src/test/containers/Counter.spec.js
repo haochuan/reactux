46451cd7df90b127328b0c5cc0d29140b3be20e0
Haochuan Liu <haochuan.liu@gmail.com>
2016-06-04 14:20:39 -0700

    wrap up component test

--- template/src/test/containers/Counter.spec.js
+++ template/src/test/containers/Counter.spec.js
@@ -1,76 +1,38 @@
 import test from 'tape'
+import expect from 'expect'
 import React from 'react'
 import { shallow } from 'enzyme'
 import { Counter } from '../../containers/Counter'
 
+function setupActions() {
+    return {
+        increment: expect.createSpy(),
+        decrement: expect.createSpy()
+    }
+}
+
 
 test('The init display count should be 0', (it) => {
-    const wrapper = shallow(<Counter value='0'/>)
+    const wrapper = shallow(<Counter value={ 0 }/>);
     it.plan(1);
     it.equal(wrapper.find('span').text(), '0');
 });
 
+test('The counter display should be based on the props value', (it) => {
+    const wrapper = shallow(<Counter value={ 10 }/>);
+    it.plan(1);
+    it.equal(wrapper.find('span').text(), '10');
+});
 
-// function setup(value = 0) {
-//   const actions = {
-//     onIncrement: expect.createSpy(),
-//     onDecrement: expect.createSpy()
-//   }
-//   const component = shallow(
-
-//         <Counter />
-//   )
-
-//   return {
-//     component: component,
-//     actions: actions,
-//     buttons: component.find('button'),
-//     p: component.find('p')
-//   }
-// }
-
-// describe('Counter component', () => {
-//   it('should display count', () => {
-//     const { p } = setup()
-//     expect(p.text()).toMatch(/^Clicked: 0 times/)
-//   })
-
-//   it('first button should call onIncrement', () => {
-//     const { buttons, actions } = setup()
-//     buttons.at(0).simulate('click')
-//     expect(actions.onIncrement).toHaveBeenCalled()
-//   })
-
-//   it('second button should call onDecrement', () => {
-//     const { buttons, actions } = setup()
-//     buttons.at(1).simulate('click')
-//     expect(actions.onDecrement).toHaveBeenCalled()
-//   })
-
-//   it('third button should not call onIncrement if the counter is even', () => {
-//     const { buttons, actions } = setup(42)
-//     buttons.at(2).simulate('click')
-//     expect(actions.onIncrement).toNotHaveBeenCalled()
-//   })
-
-//   it('third button should call onIncrement if the counter is odd', () => {
-//     const { buttons, actions } = setup(43)
-//     buttons.at(2).simulate('click')
-//     expect(actions.onIncrement).toHaveBeenCalled()
-//   })
-
-//   it('third button should call onIncrement if the counter is odd and negative', () => {
-//     const { buttons, actions } = setup(-43)
-//     buttons.at(2).simulate('click')
-//     expect(actions.onIncrement).toHaveBeenCalled()
-//   })
-
-//   it('fourth button should call onIncrement in a second', (done) => {
-//     const { buttons, actions } = setup()
-//     buttons.at(3).simulate('click')
-//     setTimeout(() => {
-//       expect(actions.onIncrement).toHaveBeenCalled()
-//       done()
-//     }, 1000)
-//   })
-// })
+// TODO: test the button click event
+// Now the click in the component will call this.dispatch, 
+// but in the test there is no way to get that,
+// Better to just call this.props.somefunction for the onClick
+// 
+// 
+// test('Increase button will call action.increment()', (it) => {
+//     const wrapper = shallow(<Counter value={ 10 }/>);
+//     const actions = setupActions();
+//     const button = wrapper.find('button').at(0).simulate('click');
+//     expect(actions.onIncrement).toHaveBeenCalled();
+// });
