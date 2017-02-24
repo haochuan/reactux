c37622a3c0e97e868a6de0d5c78a01f902108c13
Haochuan Liu <haochuan.liu@gmail.com>
2017-02-24 11:46:55 -0800

    move all redux setting togother

--- template/frontend/src/index.js
+++ template/frontend/src/index.js
@@ -4,18 +4,29 @@
  *
  */
 
+import React from "react";
+import ReactDOM from "react-dom";
+import { configureStore } from "./configureStore";
+import { Router, Route, hashHistory } from "react-router";
+import { Provider } from "react-redux";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { configureStore } from './configureStore';
-import Root from './containers/Root';
+import Counter from "./containers/Counter";
+import TabOne from "./components/TabOne";
+import TabTwo from "./components/TabTwo";
 
 const store = configureStore();
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 
 function render() {
   ReactDOM.render(
-    <Root store={store} />,
+    <Provider store={store}>
+      <Router history={hashHistory}>
+        <Route name="home" path="/" component={Counter}>
+          <Route path="tabone" component={TabOne} />
+          <Route path="tabtwo" component={TabTwo} />
+        </Route>
+      </Router>
+    </Provider>,
     rootEl
   );
 }
