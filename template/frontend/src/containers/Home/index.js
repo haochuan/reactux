6326c8ad24a2a0eeb3934c1b6720351b021f7f29
Haochuan Liu <haochuan.liu@gmail.com>
2017-03-07 16:02:26 -0800

    basic react router setting

--- template/frontend/src/containers/Home/index.js
+++ template/frontend/src/containers/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 import { Button } from 'antd';
 import './style.css';
 
@@ -7,13 +8,35 @@ export class Home extends Component {
   constructor(props) {
     super(props);
   }
+
+  goLogin() {
+    browserHistory.push('/login');
+  }
+
+  goSignup() {
+    browserHistory.push('/signup');
+  }
+
+  goDashboard() {
+    browserHistory.push('/dashboard');
+  }
+
+  goNotFound() {
+    browserHistory.push('/notFound');
+  }
+
   render() {
     const { value } = this.props;
     return (
       <div>
         This is the home page route.
         <div>
-          <Button type="primary">Login</Button>
+          <Button type="primary" onClick={this.goLogin}>Login</Button>
+          <Button type="primary" onClick={this.goSignup}>Signup</Button>
+          <Button type="primary" onClick={this.goDashboard}>Dashboard</Button>
+          <Button type="primary" onClick={this.goNotFound}>
+            Non Existed Page
+          </Button>
         </div>
       </div>
     );
