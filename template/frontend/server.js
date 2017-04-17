31613aa3ef3ac0bc9a27767659bd2e0dbb2bcc62
Haochuan Liu <haochuan.liu@gmail.com>
2017-04-17 16:16:22 -0700

    update frontend

--- template/frontend/server.js
+++ template/frontend/server.js
@@ -5,21 +5,61 @@ var favicon = require('serve-favicon');
 var compression = require('compression');
 // webpack
 var path = require('path');
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
-
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var webpackConfig = require('./webpack.config');
+var historyApiFallback = require('connect-history-api-fallback');
 var app = express();
 
 app.use(compression());
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(favicon(__dirname + '/favicon.ico'));
 
-var port = isProduction ? process.env.PORT : 3000;
 var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
 
-app.use(express.static(path.join(__dirname, '/build')));
-app.get('*', function response(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+if (isProduction) {
+  app.use(express.static(path.join(__dirname, '/build')));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+} else {
+  var compiler = webpack(webpackConfig);
+  var webpackMiddleware = webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    quite: true,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
+  // =========================
+  //   // use historyApiFallback so that routing in SPA works (YO)
+  app.use(
+    historyApiFallback({
+      verbose: false
+    })
+  );
+  // =========================
+  app.use(webpackMiddleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.get('/', function response(req, res) {
+    res.write(
+      webpackMiddleware.fileSystem.readFileSync(
+        path.join(__dirname, './dist/index.html')
+      )
+    );
+    res.end();
+  });
+}
 
 /*===========================================
 =            Baic Authentication            =
