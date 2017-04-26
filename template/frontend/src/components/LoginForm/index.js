5f79613128009e1d40d9c8d8899b6318c3c4550b
Haochuan Liu <haochuan.liu@gmail.com>
2017-04-26 13:52:19 -0700

    rewriting login page and auth logic

---
+++ template/frontend/src/components/LoginForm/index.js
@@ -0,0 +1,80 @@
+import React, { Component, PropTypes } from 'react';
+import { Form, Icon, Input, Button } from 'antd';
+import './style.css';
+const FormItem = Form.Item;
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+class HorizontalLoginForm extends Component {
+  componentDidMount() {
+    // To disabled submit button at the beginning.
+    this.props.form.validateFields();
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.props.submitLoginHandler(values.userName, values.password);
+      }
+    });
+  };
+  render() {
+    const {
+      getFieldDecorator,
+      getFieldsError,
+      getFieldError,
+      isFieldTouched
+    } = this.props.form;
+
+    // Only show error after a field is touched.
+    const userNameError =
+      isFieldTouched('userName') && getFieldError('userName');
+    const passwordError =
+      isFieldTouched('password') && getFieldError('password');
+    return (
+      <Form layout="inline" onSubmit={this.handleSubmit}>
+        <FormItem
+          validateStatus={userNameError ? 'error' : ''}
+          help={userNameError || ''}
+        >
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ fontSize: 13 }} />}
+              placeholder="Username"
+            />
+          )}
+        </FormItem>
+        <FormItem
+          validateStatus={passwordError ? 'error' : ''}
+          help={passwordError || ''}
+        >
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ fontSize: 13 }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </FormItem>
+        <FormItem>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={hasErrors(getFieldsError())}
+          >
+            Log in
+          </Button>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+const WrappedHorizontalLoginForm = Form.create()(HorizontalLoginForm);
+export default WrappedHorizontalLoginForm;
